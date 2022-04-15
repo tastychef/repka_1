1a1ebd6d015d24849e7ce8c5b268a3607e6147b5
Alex Alex <102162709+tastychef@users.noreply.github.com>
2022-04-15 07:02:47 +0300

    день 1-й

---
+++ while-for.py
@@ -0,0 +1,22 @@
+# a = 100
+#
+# while a > 10:
+#     print (a)
+#     a-=10
+# //////////////////////////////////////////
+# count = 1  # фиксируем начальное значение
+# while count <= 10:  # и конечное (включительно)
+#     print(count)
+#     count += 1
+
+# counter = 11
+# while True:
+#     if counter == 10:
+#         break
+#     counter += 1
+#
+# name = "ALEX "
+# count = 0
+# for i in name:
+#     count +=1
+# print (count)
