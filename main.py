9ddd81235e7a77a7e64b2a36b786279386d27315
Alex Alex <102162709+tastychef@users.noreply.github.com>
2022-04-22 06:23:04 +0300

    Расчет простого числа

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
-a = "A"
-b = "B"
-print (f"{a} и {b} сидели на трубе! \n.{a} - упала, {b} - пропала. Кто остался убирать?")
+num = int(input("Введите число: "))
+simple = True
+i = 2
+while i <= num // 2:
+    print(i, simple)
+    if num % i == 0:
+        simple = False
+        break
+    i += 1
+if simple:
+    print("Это простое число")
+else:
+    print("Это сложное число")
+
