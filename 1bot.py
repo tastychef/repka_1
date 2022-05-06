d45b270083eb4c37811b24b4bf2fb6088ab7eb30
Alex Alex <102162709+tastychef@users.noreply.github.com>
2022-05-06 07:28:56 +0300

    1_версия_бота

--- 1bot.py
+++ 1bot.py
@@ -1,14 +1,13 @@
 import telebot
+# Создаем экземпляр бота
 bot = telebot.TeleBot('5393953243:AAEgydRql-P3YNYHY7uBLIxUIXqC3BBsIrQ')
-
-@bot.message_handler(content_types=['text', 'document', 'audio'])
-def get_text_messages(message):
-    if message.text == "Привет":
-        bot.send_message(message.from_user.id, "Привет, чем я могу тебе помочь?")
-    elif message.text == "/help":
-        bot.send_message(message.from_user.id, "Напиши привет")
-    else:
-        bot.send_message(message.from_user.id, "Я тебя не понимаю. Напиши /help.")
-
-
+# Функция, обрабатывающая команду /start
+@bot.message_handler(commands=["start"])
+def start(m, res=False):
+    bot.send_message(m.chat.id, 'Я на связи. Напиши мне что-нибудь )')
+# Получение сообщений от юзера
+@bot.message_handler(content_types=["text"])
+def handle_text(message):
+    bot.send_message(message.chat.id, 'Вы написали: ' + message.text)
+# Запускаем бота
 bot.polling(none_stop=True, interval=0)
