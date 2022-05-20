cbb6af7c3aa7c2f94367f761c1f31de0bb73e67c
Alex Alex <102162709+tastychef@users.noreply.github.com>
2022-05-20 17:06:47 +0300

    2

---
+++ BAIKONUR.py
@@ -0,0 +1,4 @@
+# for i in range(3):
+#     q = int(input())
+#     print((i+1)*q)
+    
---
+++ Цикл for.py
@@ -0,0 +1,4 @@
+aliceQuote = "The best way to explain it is to do it."
+for letter in aliceQuote:
+    asd += 1
+print(letter)
